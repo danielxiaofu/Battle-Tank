14397910e3f6c37bf7d6480d342f10ea00cdc0a6
DESKTOP-5VO1072\yangy <yangyixiao93@gmail.com>
2016-09-02 18:39:35 -0400

    Implement barrel class to blueprint

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -8,12 +8,21 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
 public:
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreePerSecond = 20; // Sensible defaults
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxElevation = 20;
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MinElevation = 0;
 
 	void Elevate(float DegreesPerSecond);
 	
