f1142307088278c6e98803f468209798661bdc41
DESKTOP-5VO1072\yangy <yangyixiao93@gmail.com>
2016-09-05 10:53:15 -0400

    Make tank move forward and backward

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,14 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "../Public/TankTrack.h"
 #include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet) {
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
 
 void UTankMovementComponent::IntendMoveBackward(float Throw) {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move backward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -5,8 +5,9 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
 /**
- * 
+ * Responsible for driving the tank tracks
  */
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
@@ -15,10 +16,17 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	
 public:
 
+UFUNCTION(BlueprintCallable, Category = Setup)
+void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
 UFUNCTION(BlueprintCallable, Category = Input)
 void IntendMoveForward(float Throw);
 	
 UFUNCTION(BlueprintCallable, Category = Input)
 void IntendMoveBackward(float Throw);
 
+private:
+
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 };
